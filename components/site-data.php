e7e179aa6103582fdf06a241899a4a4c0e89a862
Ramon <ramonp233@gmail.com>
2014-09-30 23:04:02 +0300

    Lisaülesanne tehtud ja FINISHED

--- components/site-data.php
+++ components/site-data.php
@@ -3,9 +3,11 @@
 
   $site_title = "Arvestustöö";
 
-  $menu_main = array("Avaleht" => "index.php", "Uudised" => "news.php", "Meeskond" => "team.php", "Harjutused" => "exercises.php");
+  $menu_main = array("Avaleht" => "index.php", "Uudised" => "news.php", "Meeskond" => "team.php", "Harjutused" => "exercises.php", "Lisaülesanne" => "lisa.php");
 
   $index_content = "<h2>Tere!</h2><p>Eesti Vabariik on riik Põhja-Euroopas. Eesti piirneb põhjas üle Soome lahe Soome Vabariigiga, läänes üle Läänemere Rootsi Kuningriigiga, lõunas Läti Vabariigiga ja idas Venemaa Föderatsiooniga. Eesti pindala on tänapäeval 45 227 ruutkilomeetrit, Teise maailmasõja eel oli see praegusest suurem.[5] Kaugemas ajaloos oli nüüdne Eesti Vabariigi territoorium üks osa Liivimaa territooriumist ning kuulus osaliselt või täielikult Taani, Rootsi, Saksa ja Vene riikide koosseisu.</p><p>Tänapäeval on Eesti demokraatlik parlamentaarne vabariik. Eesti Vabariigi territoorium on jagatud viieteistkümneks maakonnaks. Iga maakond on omakorda jagatud valdadeks. Suurim linn on pealinn Tallinn. 1,29 miljoni elanikuga on Eesti Euroopa Liidu üks väiksema elanikkonnaga riike. Eesti oli 22. septembrist 1921 Rahvasteliidu liige ning kuulub 17. septembrist 1991 Ühinenud Rahvaste Organisatsiooni ja OSCEsse, 13. novembrist 1999 WTOsse, 29. märtsist 2004 NATOsse, 1. maist 2004 Euroopa Liitu, 21. detsembrist 2007 Schengeni ruumi, 9. detsembrist 2010 OECDsse ja 1. jaanuarist 2011 euroalasse. Eesti rahaühik on euro. Eesti on alla kirjutanud keskkonnakaitsealasele Kyōto protokollile.</p>";
+
+  $lisa_content = "<p>NB! See ülesanne ei ole kohustuslik ning seda ei ole me otseselt õppinud, kuid kui see õnnestub ära lahendada, siis saab selle eest lisapunkte, mis võivad hinnet tõsta, kui mõni eelnevatest ülesannetest on poolikult või ebaloogiliselt lahendatud.</p>";
 
   $footer_content = "Arvestustöö: Ramon Pääsuke - 30.09.2014";
 
---
+++ layouts/lisa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include "../components/html-head.php"; ?>
+</head>
+
+<body>
+<div id="site-container">
+    <?php include "../components/site-header.php"; ?>
+
+    <main id="site-body">
+        <section id="site-content">
+            <?php
+            echo $lisa_content;
+            ?>
+        </section>
+    </main>
+
+    <?php include "../components/site-footer.php"; ?>
+</div>
+</body>
+</html>
